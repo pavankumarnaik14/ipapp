e5d5a52c049b0b77120681cbb50ac198ec3592c3
pavankumarnaik14 <56307723+pavankumarnaik14@users.noreply.github.com>
2019-10-11 15:07:29 +0530

    update regex

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import json
 class TestApp(unittest.TestCase):
     def test_remote_addr(self):
         hostname = json.load(urlopen('http://ipinfo.io/json'))
-        self.assertRegex(hostname['ip'],r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')
+        self.assertRegex(hostname['ip'],r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
 
 if __name__ == '__main__':
     unittest.main()
